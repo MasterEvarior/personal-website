bf591609e6c1b4bd9430b3ffa930bac7bb92c312
MasterEvarior <masterevarior@protonmail.com>
2021-01-03 14:33:02 +0100

    Added vue-shell

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,75 @@
 <template>
   <div id="app">
+    <v-shell
+      :banner="banner"
+      :shell_input="send_to_terminal"
+      :commands="commands"
+      @shell_output="prompt"
+    ></v-shell>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      send_to_terminal: "",
+      banner: {
+        header: "Giannin",
+        subHeader: "Welcome to my website, enjoy your stay 🏝",
+        helpHeader: 'Type "help" for more information.',
+        emoji: false,
+        sign: `admin$`,
+        img: {
+          align: "left",
+          link: "https://github.com/MasterEvarior.png",
+          width: 100,
+          height: 100
+        }
+      },
+      commands: [
+        {
+          name: "about",
+          desc: "uwu",
+          get(){
+            return "Hi, I'm Giannin.\n" +
+            "I'm a developer in the small country of Switzerland🇨🇭 and do most of my work in a Java☕️ environment. Though I also enjoy working with other technologies💾 such as Vue.js, Docker and more."
+          }
+        },
+        {
+          name: "contact",
+          desc: "",
+          get() {
+            return "GitHub: https://github.com/MasterEvarior\n" +
+                   "Email: mailto:contact@giannin.dev"
+          }
+        }
+      ]
+    };
+  },
+  methods: {
+    prompt(value) {
+      if (value.trim() === "ifconfig") {
+        this.send_to_terminal = `
+    Wi-Fi wireless network card:
+        
+    Local link IPv6 address. . . : fe80 :: 340f: 6f02: 41e9: 477b% 24
+    IPv4 address. . . . . . . . .: 192.168.1.2
+    Subnet mask. . . . . . . . . : 255.255.255.0
+    Default Gateway. . . . . . . : 192.168.1.1`;
+      } else if(value.trim() === "pwd"){
+        this.send_to_terminal = '/Users/Giannin'
+      } else {
+        this.send_to_terminal = `'${value}' is not recognized as an internal or external command, an executable program or a batch file. Type "help" for a list of commands`;
+      }
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    background-color: black;
+  }
 </style>
